5f0abe6e0eb918ac0717090109e59a3c6857021b
Сергей Савельев <carrion19870317@gamil.com>
2023-04-10 12:14:35 +0300

    Подключаем базу данных для USERS

--- project/apps/users/src/app/authentication/rdo/user.rdo.ts
+++ project/apps/users/src/app/authentication/rdo/user.rdo.ts
@@ -1,4 +1,4 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserRdo {
@@ -6,7 +6,9 @@ export class UserRdo {
     description: 'The uniq user ID',
     example: '13'
   })
+
   @Expose({ name: '_id'})
+  @Transform(({obj}) => obj._id.toString())
   public id: string;
 
   @ApiProperty({
