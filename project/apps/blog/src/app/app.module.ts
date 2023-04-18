dd3bc28bd97d59f03bd7ad1a24c116991dc3d68f
Сергей Савельев <79596804+zuxzux@users.noreply.github.com>
2023-04-18 10:02:37 +0300

    Написали функционал для приложения Blog

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { BlogCategoryModule } from './blog-category/blog-category.module';
+import { BlogPostModule } from './blog-post/blog-post.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, BlogCategoryModule, BlogPostModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
